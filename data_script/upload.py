dec69d068860b0345bbc5876a1e17c703b585c6b
Nicholas Bardy <nicholasbardy@gmail.com>
2023-08-15 16:06:28 -0700

    add upload folders to hf

---
+++ data_script/upload.py
@@ -0,0 +1,241 @@
+import os
+import re
+from PIL import Image as PILImage
+import pandas as pd
+import argparse
+import shutil
+import subprocess
+import json
+
+from huggingface_hub import create_repo
+
+org = "facet"
+
+PROGRESS_FILE = "progress.json"
+
+# Directory containing the images
+image_dirs = {
+    "super image": "/Users/nicholasbardy/Downloads/Users/nicholasbardy/Downloads",
+    "modern": "/Users/nicholasbardy/Desktop/datasets/unsplash_with_labels/images_labeled",
+    "super product image": "/Users/nicholasbardy/Downloads/super_images_apr_18",
+    "super basic image": "/Users/nicholasbardy/Desktop/datasets/SDXL 1.0 search images/super_aug",
+    "super special image": "/Users/nicholasbardy/Desktop/datasets/SDXL 1.0 search images/all",
+    "exciting super contest image": "/Users/nicholasbardy/Desktop/sdxl_comp_images/exciting",
+    "pro super contest image": "/Users/nicholasbardy/Desktop/sdxl_comp_images/pro",
+}
+
+
+image_dirs2 = image_dirs.copy()
+more = {
+    "normal render": "Facet_SD_Dataset/Behance/Style/",
+    "high_fashion": "Facet_SD_Dataset/Zara",
+    "architecture": "Facet_SD_Dataset/Minimalissimo/Architecture/",
+    "model": "Facet_SD_Dataset/Loewe/Person",
+    "design": "Facet_SD_Dataset/Dribbble",
+    "engineering": "Facet_SD_Dataset/TeenageEngineering/Product",
+    "minimal": "Facet_SD_Dataset/Minimalissimo/",
+    "photo": "Facet_SD_Dataset/Unsplash",
+    "photo product": "Facet_SD_Dataset/Unsplash/Product",
+    "cinema": "moviestills/data/evanerichards",
+}
+
+# more
+
+
+def save_progress(progress):
+    with open(PROGRESS_FILE, "w") as f:
+        json.dump(progress, f)
+
+
+def load_progress():
+    if os.path.exists(PROGRESS_FILE):
+        with open(PROGRESS_FILE, "r") as f:
+            return json.load(f)
+    return None
+
+
+def update_progress(newp):
+    progress = load_progress()
+    if progress:
+        progress.update(newp)
+        save_progress(progress)
+
+
+# Function to check if the git repository exists
+def git_repo_exists(username, dataset_name):
+    try:
+        # Try to clone the repository
+        subprocess.run(f"git clone https://huggingface.co/{username}/{dataset_name}", shell=True, check=True)
+        # If cloning is successful, repository exists, so delete the cloned repository
+        shutil.rmtree(dataset_name)
+        return True
+    except subprocess.CalledProcessError:
+        # If cloning fails, repository does not exist
+        return False
+
+
+# Should create file if needed other wise append the line,
+# add csv header image, text
+def add_line_to_metadata(parent_folder, filename, label):
+    # Create the metadata file if it doesn't exist
+    if not os.path.exists(os.path.join(parent_folder, "metadata.csv")):
+        with open(os.path.join(parent_folder, "metadata.csv"), "w") as f:
+            f.write("image,text\n")
+
+    # Append the line
+    with open(os.path.join(parent_folder, "metadata.csv"), "a") as f:
+        f.write(f"{filename},{label}\n")
+
+    return True
+
+
+## All images should get jpg, jpeg, png, webp and do it nested if needed also ignore caps for file extensions
+# return sequable
+def all_image_iterable(image_dir):
+    for f in os.listdir(image_dir):
+        full_path = os.path.join(image_dir, f)
+
+        # Skip files that start with a period or are .DS_Store
+        if f.startswith(".") or f.endswith(".DS_Store"):
+            continue
+
+        # If the current item is a directory, yield from it recursively
+        if os.path.isdir(full_path):
+            yield from all_image_iterable(full_path)
+        elif re.search(r"\.(jpg|jpeg|png|webp)$", f, re.IGNORECASE):  # Check the file extension
+            yield full_path
+
+
+def copy_if_not_exists(src, dest):
+    if os.path.exists(dest) and os.path.getsize(src) == os.path.getsize(dest):
+        return False  # File already exists and has the same size
+    os.makedirs(os.path.dirname(dest), exist_ok=True)  # Make sure the directories exist
+    shutil.copy(src, dest)
+    return True
+
+
+# Split size
+SPLIT_SIZE = 1000
+
+# Command-line arguments
+parser = argparse.ArgumentParser(description="Process images and create a dataset.")
+parser.add_argument("--dataset_name", type=str, required=True, help="Name of the dataset")
+parser.add_argument("--username", type=str, required=True, help="Username for hugging face")
+# use dir name as tag
+parser.add_argument("--use_dir_name", action="store_true", help="Use directory name as tag")
+args = parser.parse_args()
+
+default_tags = ["image"]
+
+# Load existing progress
+progress = load_progress()
+
+
+SPLIT_SIZE = 1000
+
+parser = argparse.ArgumentParser(description="Process images and create a dataset.")
+parser.add_argument("--dataset_name", type=str, required=True, help="Name of the dataset")
+parser.add_argument("--username", type=str, required=True, help="Username for hugging face")
+parser.add_argument("--use_dir_name", action="store_true", help="Use directory name as tag")
+args = parser.parse_args()
+
+default_tags = ["image"]
+progress = load_progress()
+
+
+def make_dataset():
+    for category, image_dir in image_dirs.items():
+        if progress and category == progress["category"]:
+            start_idx = progress["index"]
+        else:
+            start_idx = 0
+
+            # Skip images until the start index if needed
+        all_images = all_image_iterable(image_dir)
+
+        # Resume by skipping the generator
+        for _ in range(start_idx):
+            next(all_images, None)
+
+        for idx, image_path in enumerate(all_images):
+            idx = idx + start_idx
+            if idx % 10 == 9:
+                save_progress({"category": category, "index": idx + 1})
+
+            full_image_path = os.path.join(image_dir, image_path)
+
+            # Read image using Pillow
+            image_pil = PILImage.open(full_image_path)
+
+            # Tags and labeling code
+            image_tags = []  # Process tags as per requirements
+
+            image_tags.append(default_tags)
+            if args.use_dir_name:
+                image_tags.append(category)
+
+            image_tags.extend(default_tags)
+            flat_image_tags = [item for sublist in image_tags for item in sublist]
+            text = " ".join(flat_image_tags)
+
+            # we need to make directory of a max image size for easier handling
+            # so we add uniform slices
+            slice_folder = f"slice_{idx//SPLIT_SIZE}"
+            slice_path = os.path.join(args.dataset_name, category, slice_folder)
+
+            relative_image_path = os.path.relpath(image_path, image_dir)  # Get the relative path
+            dest_path = os.path.join(slice_path, relative_image_path)
+
+            # Copy files and split into slices
+            if copy_if_not_exists(full_image_path, dest_path):
+                add_line_to_metadata(slice_path, dest_path, text)
+
+            # Close the image
+            image_pil.close()
+
+
+# check that not done
+if progress.get("make_dataset", False):
+    print("Already made dataset")
+else:
+    print("Making dataset")
+    make_dataset()
+
+    print("Done creating dataset")
+    print("uploading to huggingface")
+    # names
+    print(f"dataset name: {args.dataset_name}")
+    print(f"username: {args.username}")
+
+    update_progress({"make_dataset": True, "start_upload": 0})
+
+
+# Create the repository
+if progress.get("create_repo", False):
+    # in python now
+    create_repo(org + "/" + args.dataset_name, private=True)
+
+    update_progress({"make_dataset": True, "start_upload": 0, "create_repo": True})
+
+# with multline string for each command using line breaks
+if progress.get("upload", False):
+    # if not git init
+    if subprocess.run(f"cd {args.dataset_name} && git status", shell=True).returncode != 0:
+        subprocess.run(f"cd {args.dataset_name} && git init && git lfs install", shell=True)
+
+    subprocess.run(
+        f"""
+        cd {args.dataset_name}
+        git remote add origin https://huggingface.co/datasets/{args.username}/{args.dataset_name}
+        git pull origin main
+        git lfs track *
+        git add .
+        git commit -m 'Add files'
+        """,
+        shell=True,
+    )
+
+    # git push
+    subprocess.run(f"cd {args.dataset_name} && git push origin main", shell=True)
+
+    update_progress({"make_dataset": True, "start_upload": 0, "create_repo": True, "upload": True})
