6565af8bc1a7400225246eed11991e25d742a2f5
nicholasbardy <nicholasbardy@gmail.com>
2023-10-26 23:03:28 +0000

    update

--- examples/text_to_image/train_text_to_image.py
+++ examples/text_to_image/train_text_to_image.py
@@ -212,6 +212,7 @@ def parse_args():
     parser.add_argument(
         "--dataset_name",
         type=str,
+        nargs="+",
         default=None,
         help=(
             "The name of the Dataset (from the HuggingFace hub) to train on (could be your own, possibly private,"
--- examples/text_to_image/train_text_to_image_sdxl.py
+++ examples/text_to_image/train_text_to_image_sdxl.py
@@ -540,6 +540,79 @@ def parse_args(input_args=None):
 
     return args
 
+def get_train_dataset(args, accelerator):
+    # Get the datasets: you can either provide your own training and evaluation files (see below)
+    # or specify a Dataset from the hub (the dataset will be downloaded automatically from the datasets Hub).
+
+    # In distributed training, the load_dataset function guarantees that only one local process can concurrently
+    # download the dataset.
+    if args.dataset_name is not None:
+        # Downloading and loading all datasets from the hub.
+        datasets = [
+            load_dataset(name, args.dataset_config_name, cache_dir=args.cache_dir)
+            for name in args.dataset_name
+        ]
+        datasets = [d["train"] for d in datasets]
+
+        # print len of each dataset
+        print("length all")
+        for d in datasets:
+            print(len(d))
+
+        dataset = concatenate_datasets(datasets)
+
+        print("length concat")
+        print(len(dataset))
+
+    else:
+        if args.train_data_dir is not None:
+            dataset = load_dataset(
+                args.train_data_dir,
+                cache_dir=args.cache_dir,
+            )
+
+    # Preprocessing the datasets.
+    # We need to tokenize inputs and targets.
+    column_names = dataset.column_names
+
+    # 6. Get the column names for input/target.
+    if args.image_column is None:
+        image_column = column_names[0]
+        logger.info(f"image column defaulting to {image_column}")
+    else:
+        image_column = args.image_column
+        if image_column not in column_names:
+            raise ValueError(
+                f"`--image_column` value '{args.image_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            )
+
+    if args.caption_column is None:
+        caption_column = column_names[1]
+        logger.info(f"caption column defaulting to {caption_column}")
+    else:
+        all_have_key = True
+        for d in datasets:
+            if not has_caption_column(d[0], args):
+                all_have_key = False
+            
+        if not all_have_key:
+            raise ValueError(
+                f"`--caption_column` value '{args.caption_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            )
+
+    if args.conditioning_image_column is None:
+        conditioning_image_column = column_names[2]
+        logger.info(f"conditioning image column defaulting to {conditioning_image_column}")
+    else:
+        conditioning_image_column = args.conditioning_image_column
+        if conditioning_image_column not in column_names:
+            raise ValueError(
+                f"`--conditioning_image_column` value '{args.conditioning_image_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            )
+
+    train_dataset = get_train_dataset(args, accelerator)
+    return train_dataset
+
 
 # Adapted from pipelines.StableDiffusionXLPipeline.encode_prompt
 def encode_prompt(batch, text_encoders, tokenizers, proportion_empty_prompts, caption_column, is_train=True):
@@ -842,14 +915,7 @@ def main(args):
     # In distributed training, the load_dataset function guarantees that only one local process can concurrently
     # download the dataset.
     if args.dataset_name is not None:
-        # Downloading and loading a dataset from the hub.
-        all_datasets = [
-            load_dataset(name, args.dataset_config_name, cache_dir=args.cache_dir)
-            for name in args.dataset_name
-        ]
-        all_datasets = [d["train"] for d in all_datasets]
-
-        dataset = concatenate_datasets(all_datasets)
+        dataset = get_train_dataset(args, accelerator)
     else:
         data_files = {}
         if args.train_data_dir is not None:
